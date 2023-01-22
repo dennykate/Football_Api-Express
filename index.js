90aee84f19589e0ee659b3bce4e24faa7d0519d2
Denny_Kate <dennykate22@gmail.com>
2023-01-23 02:45:18 +0630

    Add redirect url

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ import cors from "cors";
 
 import standingRoutes from "./routes/standing.js";
 
+const BASE_URL = "https://football-api-express.vercel.app/standing";
 const app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.send("ဆင်ပျံကြီး always on top");
+  return res.status(200).json({
+    message: "ဆင်ပျံကြီး always on top",
+    data: [
+      `${BASE_URL}/premier-league`,
+      `${BASE_URL}/laliga`,
+      `${BASE_URL}/bundesliga`,
+      `${BASE_URL}/serie-a`,
+      `${BASE_URL}/eredivise`,
+      `${BASE_URL}/league-1`,
+    ],
+  });
 });
+
 app.use("/standing", standingRoutes);
 
 app.listen(3000, () => {
